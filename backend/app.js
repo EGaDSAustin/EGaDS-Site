c926ce9b68123cd7f04236fa0f6568b7f7d46716
imcdo <ian_mcdonald@rocketmail.com>
2020-03-27 18:01:52 -0500

    docker compose for backend

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,11 @@
 const express = require('express');
 
-const HOST = "127.0.0.1";
-// const HOST = "0.0.0.0";
-const PORT = 1234;
+const PORT =8080;
 const app = express();
 
 
 app.get('/', (req, res) => res.send('hello!'));
-app.listen(HOST, PORT);
-console.log(`Starting Backend... (port ${PORT})`);
+app.listen(PORT, () => {
+    console.log(`Starting Backend... (port ${PORT})`);
+});
 
-
-/*
-Docker build coommands
-
-docker build --tag egads-backend:1.0 .
-
-docker run -it -w /usr/src/app 
-docker run -d -v ./:/usr/src/app/ --name backend egads-backend:1.0
-*/
