80391cd6e61b861e608a9766d4807aaf6f203620
Vincent Huynh <vin.huynh1@gmail.com>
2020-03-27 17:00:56 -0500

    docker file changes

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const PORT = 8080;
 const app = express();
 
 
-app.use('/', (req, res) => res.send('hello!'));
+app.get('/', (req, res) => res.send('hello!'));
 app.listen(HOST, PORT);
 console.log(`Starting Backend... (port ${PORT})`);
 
@@ -15,6 +15,7 @@ console.log(`Starting Backend... (port ${PORT})`);
 Docker build coommands
 
 docker build --tag egads-backend:1.0 .
-docker volume ls    # shows all containers
-docker run -d -v ./:/usr/app/ --name backend egads-backend:1.0
+
+docker run -it -w /usr/src/app 
+docker run -d -v ./:/usr/src/app/ --name backend egads-backend:1.0
 */
