41912dc50ccac6af6aff66d7732cc81944432682
Ryan Hardesty Lewis <rhl@utexas.edu>
2023-08-24 18:09:38 -0400

    2022 Website Upload

--- backend/routes/login.js
+++
@@ -1,9 +0,0 @@
-const Router = require("express").Router;
-
-const login = new Router();
-
-login.post("/google", (req, res) => {
-  // needs pprovider_user_id, email, provider, provider_id, name
-});
-
-module.exports = login;
